a8731c9dcf725558520ebfa28f4076623cdaeba5
Willian Marques <1995.willianms@gmail.com>
2023-10-07 14:27:05 -0300

    Iniciando Projeto

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace terminal_clinica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Médicos
+            string nomeProfissional;
+            string especialidadeProfissional;
+            int tempoExperiencia;
+
+
+            Console.WriteLine("Quantos médicos que trabalham na clınica");
+            int numerosProfissionais = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numerosProfissionais; i++)
+            {
+                Console.WriteLine($"Qual o nome profissional {i}?");
+                nomeProfissional = Console.ReadLine();
+
+                Console.WriteLine($"Qual a especialidade de {nomeProfissional}?");
+                especialidadeProfissional = Console.ReadLine();
+
+                Console.WriteLine($"Qual o tempo de experiência de {nomeProfissional}?");
+                tempoExperiencia = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
